4a3a3dfbad3d38ff0f4bb9f039b1ee63cb6aa83e
Prashanth <Tamilanth@protonmail.com>
2024-05-11 16:01:38 +0530

    Google verification by head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="M6nxCCzJ_3vuz-HM6P0E9M_uDE5wv1IgOT72IqJ5mGY" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
